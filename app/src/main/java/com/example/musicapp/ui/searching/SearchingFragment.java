5732369d9fa107af7fb811c8f0f2b79012688a3f
feizzvan <vanvann19597@gmail.com>
2025-04-19 13:58:31 +0700

    add UI function search

--- app/src/main/java/com/example/musicapp/ui/searching/SearchingFragment.java
+++ app/src/main/java/com/example/musicapp/ui/searching/SearchingFragment.java
@@ -1,33 +1,30 @@
 package com.example.musicapp.ui.searching;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.musicapp.R;
+import com.example.musicapp.databinding.FragmentSearchingBinding;
 
 public class SearchingFragment extends Fragment {
-
+    private FragmentSearchingBinding mBinding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_searching, container, false);
+        mBinding = FragmentSearchingBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(SearchingViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+
+    }
 }
